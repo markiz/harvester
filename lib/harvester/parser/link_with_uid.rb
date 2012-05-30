82ccea8dac9ea49d1a8ffbd78f0fad2f68077da4
Mark Abramov <markizko@gmail.com>
2012-05-31 01:00:22 +0400

    Change output for H::P::LinkWithUid parser to match that of other parsers

--- lib/harvester/parser/link_with_uid.rb
+++ lib/harvester/parser/link_with_uid.rb
@@ -1,15 +1,15 @@
 module Harvester
   class Parser
     class LinkWithUid < Link
-      def parse(node)
+      def _parse(node)
         link = LinkFinder.find_matching_link(node, selectors, link_regex)
         if link
           url = link[:href]
           sliced_url = UrlManipulations.url_with_sliced_params(url, uid_keep_params)
           sliced_uid = UrlManipulations.uid_from_sliced_params(url, uid_keep_params)
           after_parse(link, {
-            url_key => sliced_url,
-            uid_key => sliced_uid
+            :url => sliced_url,
+            :uid => sliced_uid
           })
         end
       end
@@ -17,8 +17,6 @@ module Harvester
       def default_options
         {
           :uid_keep_params => :all,
-          :url_key         => :"#{name}_url",
-          :uid_key         => :"#{name}_uid",
           :link_regex      => /.+/,
           :selectors       => "a"
         }
@@ -26,14 +24,6 @@ module Harvester
 
       def uid_keep_params
         @uid_keep_params ||= options[:uid_keep_params]
-      end
-
-      def url_key
-        @url_key ||= options[:url_key]
-      end
-
-      def uid_key
-        @uid_key ||= options[:uid_key]
       end
     end
   end
--- spec/harvester/parser/link_with_uid_spec.rb
+++ spec/harvester/parser/link_with_uid_spec.rb
@@ -5,41 +5,32 @@ describe Harvester::Parser::LinkWithUid do
     subject { described_class.new(:forum, :link_regex => /forumdisplay.php/, :uid_keep_params => ["f"]) }
     let(:doc) { Nokogiri::HTML(File.read('spec/fixtures/sample_forum.html')) }
     it "returns parsed url and guid" do
-      subject.parse(doc).should == {
-        :forum_url => "forumdisplay.php?f=32",
-        :forum_uid => "32"
-      }
-    end
-
-    it "allows for returned keys redefinition" do
-      subject.options[:url_key] = :url
-      subject.options[:uid_key] = :guid
-      subject.parse(doc).should == {
-        :url  => "forumdisplay.php?f=32",
-        :guid => "32"
+      subject._parse(doc).should == {
+        :url => "forumdisplay.php?f=32",
+        :uid => "32"
       }
     end
 
     it "allows using :all for keep_params for uid generation" do
       subject.options[:uid_keep_params] = :all
-      subject.parse(doc).should == {
-        :forum_url => "forumdisplay.php?f=32",
-        :forum_uid => "forumdisplay.php?f=32"
+      subject._parse(doc).should == {
+        :url => "forumdisplay.php?f=32",
+        :uid => "forumdisplay.php?f=32"
       }
     end
 
     it "allows using :all_without_query for keep_params for uid generation" do
       subject.options[:uid_keep_params] = :all_without_query
-      subject.parse(doc).should == {
-        :forum_url => "forumdisplay.php",
-        :forum_uid => "forumdisplay.php"
+      subject._parse(doc).should == {
+        :url => "forumdisplay.php",
+        :uid => "forumdisplay.php"
       }
     end
 
     it "calls an after_parse hook" do
-      hook = proc {|node, parsed| parsed[:forum_url] = "/"; parsed }
+      hook = proc {|node, parsed| parsed[:url] = "/"; parsed }
       subject.options[:after_parse] = hook
-      subject.parse(doc)[:forum_url].should == "/"
+      subject._parse(doc)[:url].should == "/"
     end
   end
 end
