c1acb84a9a2866b894ab0c3dffb02dab7d220f73
Mark Abramov <markizko@gmail.com>
2012-05-30 21:06:32 +0400

    Basic sanity check for urls in H::P::Link

--- lib/harvester/parser/link.rb
+++ lib/harvester/parser/link.rb
@@ -1,14 +1,15 @@
+require 'uri'
+
 module Harvester
   class Parser
     class Link < Base
       def _parse(node)
         link = node.css(*selectors).
                   select {|link| link[:href] &&
+                                 valid_url?(link[:href]) &&
                                  match_any?(link[:href], link_regex) }.
                   first
-        if link && link[:href]
-          after_parse(link, link[:href])
-        end
+        after_parse(link, link[:href]) if link
       end
 
       def default_options
@@ -23,6 +24,11 @@ module Harvester
 
       def match_any?(string, regex)
         regex.any? {|r| r =~ string }
+      end
+
+      def valid_url?(url)
+        @@http_regexp ||= URI.regexp(['http', 'https'])
+        url.match(@@http_regexp).begin(0) == 0
       end
     end
   end
--- spec/harvester/parser/link_spec.rb
+++ spec/harvester/parser/link_spec.rb
@@ -14,5 +14,10 @@ describe Harvester::Parser::Link do
       subject.options[:after_parse] = hook
       subject._parse(node).should == "Github: http://github.com/markiz"
     end
+
+    it "ignores `javascript:` crap" do
+      node = Nokogiri::HTML("<body><a href='http://twitter.com/johndoe'>Twitter</a><a href='javascript:alert(\"http://github.com/markiz\")'>Github</a></body>")
+      subject._parse(node).should == nil
+    end
   end
 end
