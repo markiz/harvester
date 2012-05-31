f0a6e9a47baee540355d32a612bb31692744254f
Mark Abramov <markizko@gmail.com>
2012-05-31 12:53:19 +0400

    Add example and integration spec for a forum parser

---
+++ examples/forum_parser.rb
@@ -0,0 +1,85 @@
+$:.unshift File.expand_path("../../lib", __FILE__)
+require 'pp'
+require 'harvester'
+
+# When author link text is empty, use "Anonymous"
+author_normalization = proc do |node, author|
+  author[:name] = author[:link] && author[:link][:text].size > 0 ? author[:link][:text] : "Anonymous"
+  author
+end
+
+
+harvester = Harvester.new do |p|
+  p.element :title, :selectors => "h2 > a"
+  p.children :posts, :selectors => "div.post" do |post|
+    post.element :title, :selectors => "h3"
+    post.element :body, :selectors => "div.content"
+    post.link_with_uid :link,
+        :selectors       => "p.author > a",
+        :link_regex      => /viewtopic\.php.*p=\d+/,
+        :uid_keep_params => "p",
+        :keep_fragment   => true
+    post.child :author, :selectors => "dl.postprofile", :after_parse => author_normalization do |author|
+      author.link_with_uid :link,
+          :selectors       => "a:last-child",
+          :link_regex      => /memberlist\.php.*viewprofile/,
+          :uid_keep_params => ["mode", "u"],
+          :after_parse     => proc {|node, parsed| parsed[:text] = node.text; parsed }
+    end
+  end
+end
+
+# http://www.phpbb.com/community/viewtopic.php?t=2156784
+forum_thread = File.read('spec/fixtures/phpbb_forum_thread.html')
+
+pp harvester.parse(forum_thread)
+# {:title=>"statistics report",
+#  :posts=>
+#   [{:title=>"statistics report",
+#     :body=>
+#      "Is there any way to pull a report of statistics (e.g. active posts, content and sender) from the standard phpbb? Or do I have to install some other module to obtain this information? If so, which one? There is a lot of varieties of which some seem very old and have warnings to them from this support.",
+#     :link=>{:url=>"./viewtopic.php?p=13150823#p13150823", :uid=>"13150823"},
+#     :author=>
+#      {:link=>
+#        {:url=>"./memberlist.php?mode=viewprofile&u=1345591",
+#         :uid=>"viewprofile_1345591",
+#         :text=>"blocum"},
+#       :name=>"blocum"}},
+#    {:title=>"Re: statistics report",
+#     :body=>"Did you see phpBB statistics?",
+#     :link=>{:url=>"./viewtopic.php?p=13151077#p13151077", :uid=>"13151077"},
+#     :author=>
+#      {:link=>
+#        {:url=>"./memberlist.php?mode=viewprofile&u=719195",
+#         :uid=>"viewprofile_719195",
+#         :text=>"Mick"},
+#       :name=>"Mick"}},
+#    {:title=>"Re: statistics report",
+#     :body=>
+#      "Yes I found it but it has no specifications on where to install, what it does and in the discussion tab you can see that there is a lot of problems attached to it. As it is a year old I hesitated if I had found the latest version. Is there a module description? Is it supported by phpBB?",
+#     :link=>{:url=>"./viewtopic.php?p=13151109#p13151109", :uid=>"13151109"},
+#     :author=>
+#      {:link=>
+#        {:url=>"./memberlist.php?mode=viewprofile&u=1345591",
+#         :uid=>"viewprofile_1345591",
+#         :text=>"blocum"},
+#       :name=>"blocum"}},
+#    {:title=>"Re: statistics report",
+#     :body=>
+#      "No MODs are supported by phpBB only by the author and the community.The place to look for information or ask about the the features of a MOD is the Discussion/Support forum for the MOD.Instructions for installing MODs are in the install.xml file contained in the MOD package.For general information on installing MODs see:Installing MODs the Right Way[Tutorial] How to install a MODX modificationAnd/or Opening MODs and Installing MODsAutoMOD is an automated tool that can be used for installing most recent MODs. See also, Installing AutoMOD, and for how to use it, Installing MODs with AutoMOD.You will need to use appropriate tools to make the changes, see Tools needed to set up and customise phpBB.",
+#     :link=>{:url=>"./viewtopic.php?p=13151112#p13151112", :uid=>"13151112"},
+#     :author=>
+#      {:link=>
+#        {:url=>"./memberlist.php?mode=viewprofile&u=987265",
+#         :uid=>"viewprofile_987265",
+#         :text=>"Oyabun1"},
+#       :name=>"Oyabun1"}},
+#    {:title=>"Re: statistics report",
+#     :body=>"OK thanks for your information. I will test this.",
+#     :link=>{:url=>"./viewtopic.php?p=13151358#p13151358", :uid=>"13151358"},
+#     :author=>
+#      {:link=>
+#        {:url=>"./memberlist.php?mode=viewprofile&u=1345591",
+#         :uid=>"viewprofile_1345591",
+#         :text=>"blocum"},
+#       :name=>"blocum"}}]}
---
+++ spec/integration/forum_parser_spec.rb
@@ -0,0 +1,82 @@
+require 'spec_helper'
+
+describe Harvester do
+  describe "parsing forum section" do
+    let(:html) { File.read("spec/fixtures/phpbb_forum_section.html") }
+    let(:doc) { Nokogiri::HTML(html) }
+    subject do
+      Harvester::Parser.new do |p|
+        p.element :title, :selectors => "h2 > a"
+        p.links_with_uid :subsections,
+            :selectors       => "a.forumtitle",
+            :link_regex      => /viewforum\.php.*f=\d+/,
+            :uid_keep_params => ["f"]
+        p.links_with_uid :threads,
+            :selectors       => "a.topictitle",
+            :link_regex      => /viewtopic\.php.*t=\d+/,
+            :uid_keep_params => ["t"]
+      end
+    end
+
+    it "can gather links to subsections" do
+      result = subject.parse(doc)
+      result[:subsections].should include({:url => "./viewforum.php?f=65", :uid => "65"})
+    end
+
+    it "can gather links to threads" do
+      result = subject.parse(doc)
+      result[:threads].should include({:url => "./viewtopic.php?t=543515", :uid => "543515"})
+    end
+
+    it "can gather forum title" do
+      subject.parse(doc)[:title].should == "3.0.x Support Forum"
+    end
+  end
+
+  describe "parsing forum thread" do
+    let(:html) { File.read("spec/fixtures/phpbb_forum_thread.html") }
+    let(:doc) { Nokogiri::HTML(html) }
+    let(:author_normalization) do
+      proc do |node, author|
+        author[:name] = author[:link] ? author[:link].delete(:text) : "Anonymous"
+        author
+      end
+    end
+    subject do
+      Harvester::Parser.new do |p|
+        p.element :title, :selectors => "h2 > a"
+        p.children :posts, :selectors => "div.post" do |post|
+          post.element :title, :selectors => "h3"
+          post.element :body, :selectors => "div.content"
+          post.link_with_uid :link,
+              :selectors       => "p.author > a",
+              :link_regex      => /viewtopic\.php.*p=\d+/,
+              :uid_keep_params => "p",
+              :keep_fragment   => true
+          post.child :author, :selectors => "dl.postprofile", :after_parse => author_normalization do |author|
+            author.link_with_uid :link,
+                :selectors       => "a",
+                :link_regex      => /memberlist\.php.*viewprofile/,
+                :uid_keep_params => ["mode", "u"],
+                :after_parse     => proc {|node, parsed| parsed[:text] = node.text; parsed }
+          end
+        end
+      end
+    end
+
+    it "parses thread title" do
+      subject.parse(doc)[:title].should == "statistics report"
+    end
+
+    it "parses thread posts" do
+      posts = subject.parse(doc)[:posts]
+      posts.count.should == 5
+      posts[0][:title].should == "statistics report"
+      posts[0][:body].should include("Is there any way to pull a report")
+      posts[0][:author][:link][:url].should == "./memberlist.php?mode=viewprofile&u=1345591"
+      posts[0][:author][:link][:uid].should == "viewprofile_1345591"
+      posts[0][:author][:name].should == "blocum"
+    end
+
+  end
+end
